2276797bbd148c28495433c79fa0a2e22cb9ad0f
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-05 15:32:37 +0600

    part-06 lesson-14

---
+++ src/components/Navigation/MenuToggle/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './menuToggle.module.sass'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faTimes} from "@fortawesome/free-solid-svg-icons"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
+
+
+
+const MenuToggle = props => {
+  const csl = [
+    styles.menuToggle
+  ]
+  
+  let icon
+  if(props.isOpen){
+    csl.push('open')
+    icon = faTimes
+  
+  }else{
+    icon = faBars
+  }
+ 
+ 
+  return (
+    <div>
+      <FontAwesomeIcon
+        icon={icon}
+        className={csl.join(' ')}
+        onClick={props.onToggle}
+      />
+    </div>
+  )
+}
+
+export default MenuToggle
+
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import Quiz from '../../containers/Quiz'
+import MenuToggle from '../../components/Navigation/MenuToggle'
 import styles from './layout.module.scss'
 
 class Layout extends Component {
-
+  state = {
+    isOpen: false
+  }
+  onToggleMenuHandler = ()=>{
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  
   render() {
     return (
       <div className={styles.layout}>
+        <MenuToggle 
+          onToggle={this.onToggleMenuHandler}
+          isOpen={this.state.isOpen}          
+        />
         <main className={styles.main}>
           <Quiz/>
         </main>
