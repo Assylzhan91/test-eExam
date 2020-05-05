74d968fc851fdb035c3fcb126615dbd7e8a5d960
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-05 23:02:35 +0600

    part-08  lesson-02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz'
+import QuizList from './containers/QuizList'
+import Auth from './containers/Auth'
+import QuizCreator from './containers/QuizCreator'
 
 const App = props => {
   return (
     <Layout>
-        hello
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/quiz-creator" component={QuizCreator}/>
+        <Route path="/quiz/:id" component={Quiz}/>
+        <Route path="/" component={QuizList}/>
+      </Switch>
     </Layout>
   )
 }
---
+++ src/containers/Auth/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from "./"
+
+class Auth extends Component {
+
+  render() {
+  
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export  default Auth
