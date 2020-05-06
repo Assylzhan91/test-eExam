049da7a93d54be65981e3fcd6cf1a6ec64a24a4a
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-06 10:03:00 +0600

    part-08  lesson-03

---
+++ src/containers/QuizCreator/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from "./quizCreator.module.sass"
+
+class QuizCreator extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>QuizCreator</h1>
+      </div>
+    )
+  }
+}
+
+export  default QuizCreator
