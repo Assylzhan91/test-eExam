64a761a8dc8789537911ce7723fb256ecdd621c5
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-07 16:25:40 +0600

    part-10  lesson-03

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./spinner.module.sass"
+
+export default  props => {
+  return (
+    <div className={styles.spinner}>
+      <div/><div/>
+      <div/><div/>
+      <div/><div/>
+      <div/><div/>
+      <div/><div/>
+      <div/><div/>
+    </div>
+  )
+}
+
--- src/containers/QuizList/index.js
+++ src/containers/QuizList/index.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Spinner from '../../components/Spinner'
 import styles from "./quizList.module.sass"
 import {NavLink} from "react-router-dom"
 
 class QuizList extends Component {
   
-  
-  componentDidMount() {
-    axios.get('https://test-quiz-2bf28.firebaseio.com/quiz.json')
-      .then( (resp) => {
-
+  state ={
+    quizList: [],
+    isLoading: true
+  }
+  async componentDidMount() {
+    try {
+      const response = await axios.get('https://test-quiz-2bf28.firebaseio.com/quizes.json')
+      
+      const quizList = []
+      Object.keys(response.data).forEach((item, index)=>{
+        quizList.push({
+          id: item,
+          name: `Test №${index + 1}`
+        }) 
       })
-
+      
+      this.setState({
+        quizList,
+        isLoading: false
+      })
+      
+    }catch (e) {
+      console.log(e)
+    }
   }
 
   render() {
     return (
       <div className={styles.quizList}>
           <h1>QuizList</h1>
-          <ul>
-            {
-              [1, 2, 3, 4].map((item, index)=>{
-                return (
-                  <li key={index}>
-                    <NavLink to={'/quiz/' + item}>
-                        Test {item}
-                    </NavLink>
-                  </li>
-                );
-                
-              })
-            }
-          </ul>
+        {
+          this.state.isLoading
+          ? <Spinner/>
+          : <ul>
+              {
+                this.state.quizList.map(item=>{
+                  return (
+                    <li key={item.id}>
+                      <NavLink to={'/quiz/' + item.id}>
+                        Test {item.name}
+                      </NavLink>
+                    </li>
+                  );
+
+                })
+              }
+            </ul>
+        }  
+        
+          
       </div>
     )
   }
